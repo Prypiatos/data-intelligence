d127245d4f82c932666abd3da454981942a710ed
Jitharsanan Thiruketheeswaran <03jitharsanan@gmail.com>
2026-04-28 22:15:32 +0530

    Use synchronous write for InfluxDB client

--- src/ingestion/influx_writer.py
+++ src/ingestion/influx_writer.py
@@ -1,4 +1,5 @@
 from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 url = "http://localhost:8086"
 token = "energy-token-123"
@@ -6,23 +7,21 @@ org = "energy-org"
 bucket = "energy_telemetry"
 
 client = InfluxDBClient(url=url, token=token, org=org)
-write_api = client.write_api()
+
+# Use synchronous write to avoid internal retries
+write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
 def write_telemetry(data):
-    try:
-        point = (
-            Point("telemetry")
-            .tag("node_id", data["node_id"])
-            .field("voltage", float(data["voltage"]))
-            .field("current", float(data["current"]))
-            .field("power", float(data["power"]))
-            .field("energy_wh", float(data["energy_wh"]))
-            .time(data["timestamp"], WritePrecision.MS)
-        )
+    point = (
+        Point("telemetry")
+        .tag("node_id", data["node_id"])
+        .field("voltage", float(data["voltage"]))
+        .field("current", float(data["current"]))
+        .field("power", float(data["power"]))
+        .field("energy_wh", float(data["energy_wh"]))
+        .time(data["timestamp"], WritePrecision.MS)
+    )
 
-        write_api.write(bucket=bucket, org=org, record=point)
-        print("Inserted into InfluxDB")
-
-    except Exception as e:
-        print("InfluxDB error:", e)
+    write_api.write(bucket=bucket, org=org, record=point)
+    print("Inserted into InfluxDB")
